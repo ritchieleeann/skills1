7bf694a49726aa5500d18db6b78352fe30616319
Ritchie <user@users-MacBook-Pro-2.local>
2014-02-09 11:24:54 -0800

    Done.

--- skills1.py
+++ skills1.py
@@ -51,24 +51,47 @@ def halvesies(some_list):
     for items in some_list:
         halves.append(items/float(2))
     return halves
-print halvesies(some_list)
+#print halvesies(some_list)
 
 # Write a function that takes a list of words and returns a list of all the lengths of those words.
 def word_lengths(word_list):
-    return []
+    lengths = []
+    for items in word_list:
+        lengths.append(len(items))
+    return lengths
+#print word_lengths(word_list)
 
+numbers = [8, 2, 22, 222, 208, 4]
 # Write a function (using iteration) that sums all the numbers in a list.
 def sum_numbers(numbers):
-    return []
+    add = 0
+    for items in numbers:
+        add += items
+    return add
+#print sum_numbers(numbers)
 
 # Write a function that multiplies all the numbers in a list together.
 def mult_numbers(numbers):
-    return []
+    mult = 1
+    for items in numbers:
+        mult *= items
+    return mult
+#print mult_numbers(numbers)
 
+string_list = ["hello" , "hi" , "hola"]
 # Write a function that joins all the strings in a list together (without using the join method) and returns a single string.
 def join_strings(string_list):
-    return ""
+    joint = ""
+    for items in string_list:
+        joint += items
+    return joint
+#print join_strings(string_list)
 
 # Write a function that takes a list of integers and returns the average (without using the avg method)
 def average(numbers):
-    return None
+    add = 0
+    av = len(numbers)
+    for items in numbers:
+        add += items
+    return add / float(av)
+print average(numbers)
